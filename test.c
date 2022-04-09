5ca590835089e44e85fd15ca0a8c815c74a856f5
Daniel McCarney <daniel@binaryparadox.net>
2022-04-09 15:23:40 -0400

    wip: more test features, wasm build

--- test.c
+++ test.c
@@ -5,6 +5,19 @@
  * World! //Yeah
  */
 
+#pragma strong_types, strict_types
+#pragma foo
+
+#define YEET 0x9999
+
+#define SKEET(x, y)  ( y * x)
+
+#ifdef YEET
+  private void yeet();
+#else
+  private void no_yeet();
+#endif
+
 private functions nosave variables inherit "complex/room1";
 public variables inherit "complex/room2";
 private functions inherit "complex/room3";
@@ -28,8 +41,13 @@ struct Nested {
 
 struct Filled (Empty) {
   mapping a;
+  mapping b;
   struct Nested nest;
 };
+
+symbol foo = 'y;
+
+closure hap = lambda( ({ 'x, 'y, #'({ 10 }) }), ({ #'write, 'x, 'y }) );
 
 int x2 = x->fun();
 string z = "afunc";
@@ -40,6 +58,18 @@ mixed yeet2 = y.z();
 
 public varargs int|string|mapping foo(int a, varargs mixed* rest) {
   struct Filled instance = (<Filled> ([ :foo() ]));
+
+
+  mapping empty = ([ :5 ]);
+  mapping not_empty = ([ "foo": 5; "hey", 9, "bar": 10; "what", 0 ]);
+
+  printf("%d", 1 in empty);
+  printf("%d", ("foo" in not_empty));
+
+  struct Filled empty_instance = (<Filled>);
+
+  empty_instance->a = empty;
+  empty_instance.b = not_empty;
 
   closure inline = (:
     return $1;
@@ -105,3 +135,21 @@ mixed yyyyyyy() {
 
   return;
 }
+
+coroutine cb;
+
+async void cool() {
+  yield();
+}
+
+async int whatev(int arg = 1) {
+  yield(arg ? 99 : arg);
+}
+
+void x() {
+  await(cool());
+}
+
+int bar = await(whatev(9));
+
+
